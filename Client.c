3de77377965035a573e998c6d4a26aaf505cb27d
svg <svg@svg-XubuntuDev.(none)>
2014-05-20 23:00:26 +0200

    mise en palce git xD

---
+++ Client.c
@@ -0,0 +1,25 @@
+
+
+usage(){
+  printf("usage : cliecho adresseIP_serveur(x.x.x.x)  numero_port_serveur\n");
+}
+
+//fonction qui fera la requete afin de récupérer la liste des clients qui possède des fichiers correspondant aux mots clé
+void search(char* motsCles[])  {
+
+}
+
+
+void publish(char* nomFichier, char* motsCles[], ) {
+
+}
+
+int main(int argc,char* argv[]) {
+
+	//on veut appele client <adresse> <port>
+	if (argc != 3){
+		usage();
+		exit(1);
+	}
+
+}
